5d6cef3bbac836fbe6fe890cc14392637bf6b32b
Jumpei Arima <arijun0307@gmail.com>
2020-05-07 16:28:46 +0900

    change script name

--- run_docker_vnc.sh
+++ run_docker_vnc.sh
@@ -12,6 +12,6 @@ docker run -it --rm \
     -p 5900:5900 \
     -v $PWD/scripts/:/root/scripts/ \
     -e DISPLAY=:0 \
-    --name ros_vnc \
+    --name ros_vnc_${tag} \
     arijun/ros_vnc:${tag} \
     /bin/bash -c "/root/scripts/run_vnc.sh"
